4cf62a866198d8fdc3d78317ce20a93714183ce5
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-04-08 11:47:18 +0530

    comp

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -10,14 +10,15 @@ import {
 import { Heart, MapPinIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
-import { saveJobs } from "@/api/apiJobs";
+import { deleteJob, saveJobs } from "@/api/apiJobs";
 import useFetch from "@/hooks/use-fetch";
+import { BarLoader } from "react-spinners";
 
 const JobCard = ({
   job,
   isMyJob = false,
   savedInit = false,
-  onJobSaved = () => {},
+  onJobAction = () => {},
 }) => {
   const [saved, setSaved] = useState(savedInit);
   const {
@@ -35,7 +36,16 @@ const JobCard = ({
       user_id: user.id,
       job_id: job.id,
     });
-    onJobSaved();
+    onJobAction();
+  };
+
+  const { loading: loadingDeleteJob, fn: fnDeleteJob } = useFetch(deleteJob, {
+    job_id: job.id,
+  });
+
+  const handleDeleteJob = async () => {
+    await fnDeleteJob();
+    onJobAction();
   };
 
   useEffect(() => {
@@ -46,12 +56,16 @@ const JobCard = ({
 
   return (
     <Card className={"bg-[#241C31] flex flex-col border-none !text-white"}>
+      {loadingDeleteJob && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
       <CardHeader>
         <CardTitle className={"flex justify-between font-bold"}>
           {job.title}{" "}
           {isMyJob && (
             <Trash2Icon
               size={18}
+              onClick={handleDeleteJob}
               fill="red"
               className="text-red-400 cursor-pointer"
             />
--- src/pages/MyJobs.jsx
+++ src/pages/MyJobs.jsx
@@ -1,7 +1,29 @@
+import CreatedApplications from "@/components/CreatedApplications";
+import CreatedJobs from "@/components/CreatedJobs";
+import { useUser } from "@clerk/clerk-react";
 import React from "react";
+import { BarLoader } from "react-spinners";
 
 const MyJobs = () => {
-  return <div>My Jobs</div>;
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#6f32a8" />;
+  }
+  return (
+    <div>
+      <h1 className="font-extrabold text-5xl sm:text-7xl text-center pb-8">
+        {user?.unsafeMetadata?.role === "applicant"
+          ? "My Applications"
+          : "My Jobs"}
+      </h1>
+      {user?.unsafeMetadata?.role === "applicant" ? (
+        <CreatedApplications />
+      ) : (
+        <CreatedJobs />
+      )}
+    </div>
+  );
 };
 
 export default MyJobs;
